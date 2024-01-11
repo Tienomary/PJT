21695f18620507015a3de626912465364403eff9
Tienomary <37733721+Tienomary@users.noreply.github.com>
2024-01-11 10:18:00 +0100

    BDD

--- src/public/class/database.php
+++ src/public/class/database.php
@@ -18,7 +18,7 @@ class database{
     }
 
     public function verifyIfBddExists(){
-        if($this->getPDO()){
+        if($this->getPDO() != false){
             return true;
         }else{
             return false;
@@ -27,9 +27,14 @@ class database{
 
     private function getPDO(){
         if($this->pdo == null){
-            $pdo = new PDO('mysql:dbname='.$this->db_name.';host='.$this->db_host.'', $this->db_user, $this->db_pass);
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $this->pdo=$pdo;
+            try{
+                $pdo = new PDO('mysql:dbname='.$this->db_name.';host='.$this->db_host.'', $this->db_user, $this->db_pass);
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $this->pdo=$pdo;
+                return $this->pdo;
+            } catch (Exception $e) {
+                return false;
+            }
         }
         return $this->pdo;
     }
